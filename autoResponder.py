d16bc83ecc2334a9c2dafccaeb9752050481ef7a
Mateo Vial <vialmateovial@gmail.com>
2022-01-17 07:20:59 +0100

    rewrote commands into main.py

--- autoResponder.py
+++ autoResponder.py
@@ -11,14 +11,7 @@ from class_joueur import *
 from class_date import *
 
 
-pickle_filename = 'registre'
 
-# si le fichier pickle n'existe pas, on réinitialise la listejoueurs. sinon, on le récupère
-if not os.path.isfile(pickle_filename):
-    listejoueurs = []
-else:
-    file = open(pickle_filename, 'rb')
-    listejoueurs = pickle.load(file)
 
 prefix = '$'
 lenprefix = len(prefix)
@@ -73,132 +66,6 @@ class MyCog(commands.Cog):
         nelist = ['né', 'nez', 'nait', 'ner', 'nés', 'née', 'nées', 'nais', 'naient', 'nai', 'ney']
         if any([''.join(c for c in message.content if c.isalpha()).lower().endswith(ne) for ne in nelist]):
             await message.channel.send('gro', delete_after = 60)
-
-        
-
-        # removed for my tests
-        # assert message.channel.id in [814185273936052284, 930934616485949500]
-
-        if message.content.startswith(prefix):
-            mots = message.content[lenprefix:].split()
-
-            if mots[0] == 'hello':
-                await message.channel.send('Hello world!', delete_after = 5)
-            
-            elif mots[0] == 'help':
-                if len(mots) == 1:
-                    output = 'Liste des commandes :\n```' + '\n'.join([prefix + cmd for cmd in listecommandes]) + """```\nPour de l'aide sur une commande particulière, ``{0}help <commande>``""".format(prefix)
-                    await message.channel.send(output, delete_after = 30)
-                elif len(mots) == 2:
-                    try:
-                        output = open('help/{0}.txt'.format(mots[1]), mode = 'r', encoding = 'utf-8').read()
-                        await message.channel.send('```{0}```'.format(output.format(prefix)), delete_after = 30)
-                    except:
-                        await message.channel.send("""Cette commande n'existe pas.""", delete_after = 10)         
-                else:
-                    output = 'Usage incorrect.\n```{0}help\n{0}help <commande>```'.format(prefix)                
-
-            elif mots[0] in  ['ajouterjoueur', 'aj']:
-                if len(mots) != 9:
-                    # output = 'Usage incorrect.\n```{0}ajouterjoueur <pseudo>```'.format(prefix)
-                    output = 'Usage incorrect.\n```$ajouterjoueur <draps> <pseudo> <prénom> <twitter> <fc> <anniv> <num> <exteams>```'
-                    await message.channel.send(output, delete_after = 20)
-                else:
-                    listejoueurs.append(Joueur(
-                        draps = mots[1].split(','),
-                        pseudo = mots[2],
-                        prenom = mots[3],
-                        twitter = mots[4],
-                        fc = mots[5],
-                        anniv = Date(annee = int(mots[6][4:8]), mois = int(mots[6][2:4]), jour = int(mots[6][0:2])),
-                        num = mots[7],
-                        exteams = mots[8].split(','),                    
-                    ))
-
-                    file = open(pickle_filename, 'wb')
-                    pickle.dump(listejoueurs, file)
-
-                    if mots[2] == '/':
-                        pseud_temp = mots[3]
-                    else:
-                        pseud_temp = mots[2]
-
-                    await message.channel.send('Joueur {0} ajouté au registre.'.format(pseud_temp), delete_after = 8)
-
-            elif mots[0] in ['supprimerjoueur', 'sj']:
-                if len(mots) != 2:
-                    output = 'Usage incorrect.\n```{0}supprimerjoueur <N°>```'.format(prefix)
-                    await message.channel.send(output, delete_after = 20)
-                else:
-                    try:
-                        id_a_supprimer = int(mots[1]) # peut avoir une erreur
-                        if listejoueurs[id_a_supprimer].pseudo == None:
-                            pseud_temp = listejoueurs[id_a_supprimer].prenom
-                        else:
-                            pseud_temp = listejoueurs[id_a_supprimer].pseudo
-                        del listejoueurs[id_a_supprimer]
-
-                        file = open(pickle_filename, 'wb')
-                        pickle.dump(listejoueurs, file)
-
-                        await message.channel.send('Joueur {0} supprimé du registre.'.format(pseud_temp), delete_after = 8)
-                        await message.channel.send("""Les N° sont modifiés après la suppression d'un joueur.""", delete_after = 8)
-                    except:
-                        output = 'Usage incorrect.\n```{0}supprimerjoueur <N°>```'.format(prefix)
-                        await message.channel.send(output, delete_after = 20)
-
-            elif mots[0] == 'affiche':
-
-                for joueur in listejoueurs:
-                    await message.channel.send(joueur.affiche())
-
-            elif mots[0] in ['listejoueurs', 'lj']:
-                table = [['N°', 'Drapeaux', 'Pseudo', 'Prénom', 'Twitter', 'FC', 'Anniv', 'Num', 'Ex-teams']]
-                for i, joueur in enumerate(listejoueurs):
-                    liste_temp = [i]
-                    liste_temp += joueur.liste_affiche()
-                    table.append(liste_temp)
-                output = tabulate(table, headers="firstrow", tablefmt="simple")
-                try:
-                    await message.channel.send('```{0}```'.format(output), delete_after = 30)
-                except:
-                    if os.path.isfile('lj.txt'):
-                        os.remove('lj.txt')
-                    f = open('lj.txt', mode = 'w', encoding = 'utf-8')
-                    f.write(output)
-                    f.close()
-                    await message.channel.send(file = discord.File('lj.txt'), delete_after = 30)
-
-            elif mots[0] in ['swapjoueurs', 'swapj']:
-                if len(mots) == 3:
-                    try:
-                        i, j = int(mots[1]), int(mots[2])
-                        listejoueurs[i], listejoueurs[j] = listejoueurs[j], listejoueurs[i]
-                        await message.channel.send('Joueurs {0} et {1} échangés.'.format(i,j), delete_after = 8)
-                    except:
-                        await message.channel.send('Usage incorrect.', delete_after = 8)
-
-            elif mots[0] in ['deplacejoueur', 'deplacej']:
-                if len(mots) == 3:
-                    try:
-                        i, j = int(mots[1]), int(mots[2])
-                        joueur_temp = copy(listejoueurs[i])
-                        pseud_temp = listejoueurs[i].pseudo
-                        del listejoueurs[i]
-                        listejoueurs.insert(j, joueur_temp)
-                        await message.channel.send("""Joueur {0} déplacé à l'emplacement {1}""".format(pseud_temp, j), delete_after = 8)
-                    except:
-                        await message.channel.send('Usage incorrect.', delete_after = 8)
-                        
-            elif mots[0] == 'teps':
-                await message.channel.send(file = discord.File(random.choice(['prout/{0}'.format(file) for file in os.listdir('prout') if os.path.isfile('prout/{0}'.format(file))])), delete_after = 8)
-
-            elif mots[0] == 'stop' and message.author.id == 135495101496426496:
-                await message.channel.send('Ciao.')
-                exit()
-
-            else:
-                await message.channel.send('Commande inconnue. Tapez ``{0}help`` pour voir la liste des commandes.'.format(prefix), delete_after = 8)
 
 # Every extension should have this function
 def setup(bot):
--- main.py
+++ main.py
@@ -12,9 +12,30 @@ from class_date import *
 
 random.seed()
 
+pickle_filename = 'registre'
+# si le fichier pickle n'existe pas, on réinitialise la listejoueurs. sinon, on le récupère
+if not os.path.isfile(pickle_filename):
+    listejoueurs = []
+else:
+    file = open(pickle_filename, 'rb')
+    listejoueurs = pickle.load(file)
 
+bot = commands.Bot(command_prefix="$",  help_command=None)
 
-bot = commands.Bot(command_prefix="?",  help_command=None)
+listecommandes = [
+    'hello', 
+    'help', 
+    'ajouterjoueur', 
+    'listejoueurs', 
+    'supprimerjoueur', 
+    'teps', 
+    'swapjoueurs', 
+    'exit'
+]
+
+with open('admin.txt', mode='r', encoding='utf-8') as f:
+    adminlist = [int(line.split()[0]) for line in f.readlines()]
+    # edit admin.txt to edit admins
 
 @bot.event
 async def on_ready():
@@ -24,7 +45,135 @@ async def on_ready():
 # It won't be upload on github due to gitignore 
 with open("token.txt", "r") as f:
     TOKEN = f.read()
+
 bot.load_extension("autoResponder")
+
+# All commands
+
+@bot.command(name='hello')
+async def hello(ctx):
+    await ctx.send('Hello world!')
+
+@bot.command(name='help')
+async def help(ctx, *args):
+    if len(args) == 0:
+        output = '```'
+        for cmd in listecommandes:
+            output += '{0}{1}\n'.format(bot.command_prefix, cmd)
+        output += '```'
+    elif len(args) >= 1:
+        try:
+            with open('help/{0}.txt'.format(args[0]), encoding='utf-8') as f:
+                output = '```{0}```'.format(f.read().format(bot.command_prefix))
+        except:
+            output = '''Cette commande n'existe pas.'''
+    await ctx.send(output)
+
+@bot.command(name='ajouterjoueur', aliases=['aj'])
+async def ajouterjoueur(ctx, *args):
+    assert ctx.author.id in adminlist
+    # await ctx.send('commande ajouterjoueur lancée')
+    try:
+        listejoueurs.append(Joueur(
+            draps = args[0].split(','),
+            pseudo = args[1],
+            prenom = args[2],
+            twitter = args[3],
+            fc = args[4],
+            anniv = Date(annee = int(args[5][4:8]), mois = int(args[5][2:4]), jour = int(args[5][0:2])),
+            num = args[6],
+            exteams = args[7].split(',')
+        ))
+
+        with open(pickle_filename, 'wb') as f:
+            pickle.dump(listejoueurs, f)
+        
+        if args[1] == '/':
+            pseud_temp = args[2]
+        else:
+            pseud_temp = args[1]
+        await ctx.send('Joueur {0} ajouté au registre.'.format(pseud_temp), delete_after=10)
+    except:
+        await ctx.send('Usage incorrect', delete_after=10)
+
+@bot.command(name='supprimerjoueur', aliases=['sj'])
+async def supprimerjoueur(ctx, arg):
+    assert ctx.author.id in adminlist
+    try:
+        ind_a_supprimer = int(arg)
+        if listejoueurs[ind_a_supprimer].pseudo == None:
+            pseud_temp = listejoueurs[ind_a_supprimer].prenom
+        else:
+            pseud_temp = listejoueurs[ind_a_supprimer].pseudo
+        del listejoueurs[ind_a_supprimer]
+
+        with open(pickle_filename, 'wb') as f:
+            pickle.dump(listejoueurs, f)
+
+        await ctx.send('''Joueur {0} supprimé du registre.\n Les N° sont modifiés après la suppression d'un joueur.'''.format(pseud_temp), delete_after = 10)
+    except:
+        await ctx.send('Usage incorrecte', delete_after=10)
+
+@bot.command(name='affiche')
+async def affiche(ctx):
+    for joueur in listejoueurs:
+        await ctx.send(joueur.affiche())
+
+@bot.command(name='listejoueurs', aliases=['lj'])
+async def _listejoueurs(ctx): #underscore because listejoueurs is already the list of all players
+    table = [['N°', 'Drapeaux', 'Pseudo', 'Prénom', 'Twitter', 'FC', 'Anniv', 'Num', 'Ex-teams']]
+    for i, joueur in enumerate(listejoueurs):
+        liste_temp = [i] 
+        liste_temp += joueur.liste_affiche()
+        table.append(liste_temp)
+    output = tabulate(table, headers='firstrow', tablefmt='simple')
+    try:
+        await ctx.send('```{0}```'.format(output), delete_after=30)
+    except: # If output > 2000 or whatever 
+        if os.path.isfile('lj.txt'):
+            os.remove('lj.txt')
+        with open('lj.txt', mode='w', encoding='utf-8') as f:
+            f.write(output)
+        await ctx.send(file=discord.File('lj.txt'), delete_after=30)
+
+@bot.command(name='swapjoueurs', aliases=['swapj'])
+async def swapjoueurs(ctx, *args):
+    assert ctx.author.id in adminlist
+    try:
+        i, j = int(args[0]), int(args[1])
+        listejoueurs[i], listejoueurs[j] = listejoueurs[j], listejoueurs[i]
+        await ctx.send('Joueurs {0} et {1} échangés.'.format(i, j), delete_after=10)
+    except:
+        await ctx.send('Usgae incorrect.', delete_after=10)
+
+@bot.command(name='deplacejoueur', aliases=['dj'])
+async def deplacejoueur(ctx, *args):
+    assert ctx.author.id in adminlist
+    try:
+        # await ctx.send('deplacejoueur en cours')
+        i, j = int(args[0]), int(args[1])
+        joueur_temp = copy(listejoueurs[i])
+        if listejoueurs[i].pseudo == None:
+            pseud_temp = listejoueurs[i].prenom
+        else:
+            pseud_temp = listejoueurs[i].pseudo
+        del listejoueurs[i]
+        listejoueurs.insert(j, joueur_temp)
+        await ctx.send('''Joueur {0} déplacé à l'emplacement {1}'''.format(pseud_temp, j), delete_after=10)
+    except:
+        await ctx.send('Usage incorrect.', delete_after=10)
+
+@bot.command(name='teps')
+async def teps(ctx):
+    await ctx.send(file = discord.File(random.choice(['prout/{0}'.format(file) for file in os.listdir('prout')])), delete_after = 10)
+
+@bot.command(name='stop')
+async def stop(ctx):
+    assert ctx.author.id in adminlist
+    await ctx.send('Ciao.')
+    exit()
+
+
 bot.run(TOKEN)
 
 
