1b46f283d45c6053148982237ae6783aa99f0d2d
onepierre <clarou.p@free.fr>
2022-01-20 14:40:04 +0100

    porn bot

---
+++ secret.py
@@ -0,0 +1,25 @@
+import discord
+from discord.ext import tasks
+from discord.ext import commands
+from tabulate import tabulate
+import pickle
+import os
+import random
+import datetime
+from copy import copy
+
+from class_joueur import *
+
+
+class Secret(commands.Cog):
+    def __init__(self, bot):
+        self.bot = bot
+
+    @commands.Cog.listener()
+    async def on_ready(self):
+        print('Logged on as {0}!'.format(self.bot.user))
+
+
+# Every extension should have this function
+def setup(bot):
+    bot.add_cog(Secret(bot)) 
